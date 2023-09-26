5bdfa42a1e5e49b34f30a5cbfb039106a97b6d5e
durpdur <durpdur@gmail.com>
2023-09-25 20:44:22 -0700

    Add uploading-files folder

---
+++ uploading-files/src/main/java/com/durpdur/uploadingfiles/UploadingFilesApplication.java
@@ -0,0 +1,30 @@
+package com.durpdur.uploadingfiles;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.durpdur.uploadingfiles.storage.StorageProperties;
+import com.durpdur.uploadingfiles.storage.StorageService;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingFilesApplication.class, args);
+		
+	}
+
+	// This bean delete and re-create that folder at startup.
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+
+}
---
+++ uploading-files/upload-dir/DishName.java
@@ -0,0 +1,13 @@
+package com.ian;
+
+public class DishName {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
