b41f92244679bd305bce347e48be341250447d4c
durpdur <durpdur@gmail.com>
2023-10-01 00:54:11 -0400

    Add files via upload
    Work in progress for React Redux

---
+++ src/features/counter/counterSlice.spec.ts
@@ -0,0 +1,35 @@
+import counterReducer, {
+    CounterState,
+    increment,
+    decrement,
+    incrementByAmount,
+} from './counterSlice';
+
+describe('counter reducer', () => {
+    const initialState: CounterState = {
+        value: 3,
+        status: 'idle',
+    };
+
+    it('should handle intial state', () => {
+        expect(counterReducer(undefined, { type: 'unkown' })).toEqual({
+            value: 0,
+            status: 'idle',
+        });
+    });
+
+    it('should handle increment', () => {
+        const actual = counterReducer(initialState, increment());
+        expect(actual.value).toEqual(4);
+    });
+
+    it('should handle decrement', () => {
+        const actual = counterReducer(initialState, decrement());
+        expect(actual.value).toEqual(2);
+    });
+
+    it('shoudl handle incrementsByAmount', () => {
+        const actual = counterReducer(initialState, incrementByAmount(2));
+        expect(actual.value).toEqual(5);
+    });
+});
