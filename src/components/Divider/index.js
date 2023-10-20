7f5b75973fa814a1599578242fc5300550d0f619
durpdur <durpdur@gmail.com>
2023-10-20 15:43:59 -0400

    Add files via upload

---
+++ src/components/Divider/index.js
@@ -0,0 +1,3 @@
+import Divider from '.';
+
+export default Divider;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './style.css';
+
+export class Footer extends Component {
+    render() {
+        return (
+            <footer>Just a test footer!</footer>
+        )
+    }
+}
+
+export default Footer;
