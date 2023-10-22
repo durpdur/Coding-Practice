f7d381180ee5d924cd7d6d2d52e10ee03019cc6f
durpdur <durpdur@gmail.com>
2023-10-22 17:13:35 -0400

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          Hello <span>World</span>
+        </Alert>
+      )}
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          Alert for button
+        </Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,30 +1,36 @@
-import { Fragment } from "react";
+import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  // (items: string) => void
+  onSelectItem: (item: string) => void; // onClick
+}
 
-  if (items.length === 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No item found</p>;
-      </>
-    );
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
-  const message = items.length === 0 ? <p>No item found</p> : null;
-
-  const getMessage = () => {
-    return items.length === 0 ? <p>No item found</p> : null;
-  };
-
+  // Event handler
   return (
     <>
-      <h1>List Name</h1>
-      {getMessage()}
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
